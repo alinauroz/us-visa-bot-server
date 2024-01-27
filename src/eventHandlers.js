6d7e9d5aa40c4e9969893ae33a5c3c9112f7fea6
nauroz <alinauroze@hotmail.com>
2024-01-27 14:15:52 +0500

    save changes

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,3 +1,17 @@
-exports.handleDates = async () => {
-    
+const { get, update } = require("./db");
+
+exports.handleDates = async (dates) => {
+    const availableDates = (dates.split(",").map(date => date.trim()));
+    const dbDates = get("dates");
+    const existingDates = dbDates.flat();
+
+    const newDates = availableDates.filter(d => {
+        return existingDates.indexOf(d) === -1;
+    });
+
+    const updatedDates = dbDates.slice(-100);
+    updatedDates.push(availableDates);
+    update("dates", updatedDates);
+    console.log(newDates);
+    console.log("UpdatedDates", updatedDates);
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { EVENT_DATES } = require("./constants");
+const { EVENT_DATES, EVENT_RESCHEDULED } = require("./constants");
+const { handleDates } = require("./eventHandlers");
 require("dotenv").config();
 
 const app = express();
@@ -11,8 +12,13 @@ app.post("/bot-events", async (req, res) => {
     const { event, data } = req.body;
     console.log("Event", event, data);
     if (event === EVENT_DATES) {
-
+        handleDates(data);
     }
+    else if (event === EVENT_RESCHEDULED) {
+        console.log("Event");
+    }
+
+    return res.send({ msg: "ok" })
 });
 
 app.listen(3000);
